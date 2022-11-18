d5c67b0b573fa393bfb8910b79c9b8211389d8f5
IsabelMeow <isabelarias2018@gmail.com>
2022-11-18 12:36:22 -0500

    Adding getters and setters

--- HitTheTarget/app/src/main/java/edu/vanier/HitTheTarget/math/MainAppMath.java
+++ HitTheTarget/app/src/main/java/edu/vanier/HitTheTarget/math/MainAppMath.java
@@ -14,6 +14,112 @@ import javafx.geometry.Point2D;
 public class MainAppMath {
     
     private double x,y,vx,vy,ax,ay,speed,time,angle,y0,vx0,vy0,distance;
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getVx() {
+        return vx;
+    }
+
+    public void setVx(double vx) {
+        this.vx = vx;
+    }
+
+    public double getVy() {
+        return vy;
+    }
+
+    public void setVy(double vy) {
+        this.vy = vy;
+    }
+
+    public double getAx() {
+        return ax;
+    }
+
+    public void setAx(double ax) {
+        this.ax = ax;
+    }
+
+    public double getAy() {
+        return ay;
+    }
+
+    public void setAy(double ay) {
+        this.ay = ay;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public void setTime(double time) {
+        this.time = time;
+    }
+
+    public double getAngle() {
+        return angle;
+    }
+
+    public void setAngle(double angle) {
+        this.angle = angle;
+    }
+
+    public double getY0() {
+        return y0;
+    }
+
+    public void setY0(double y0) {
+        this.y0 = y0;
+    }
+
+    public double getVx0() {
+        return vx0;
+    }
+
+    public void setVx0(double vx0) {
+        this.vx0 = vx0;
+    }
+
+    public double getVy0() {
+        return vy0;
+    }
+
+    public void setVy0(double vy0) {
+        this.vy0 = vy0;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+    
+    
     public MainAppMath(double y, double angle, double speed, double gravity) 
     {
         this.y0=y;
