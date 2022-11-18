6afe7a8f73aa8e6388d9091c06b8904b2c70bf57
Maésha Mahmud <83089310+mash3133@users.noreply.github.com>
2022-11-18 12:34:02 -0500

    mathhh
    added math

--- HitTheTarget/app/src/main/java/edu/vanier/HitTheTarget/math/MainAppMath.java
+++ HitTheTarget/app/src/main/java/edu/vanier/HitTheTarget/math/MainAppMath.java
@@ -4,10 +4,53 @@
  */
 package edu.vanier.HitTheTarget.math;
 
+import java.util.ArrayList;
+import javafx.geometry.Point2D;
+
 /**
  *
  * @author maesh
  */
 public class MainAppMath {
     
+    private double x,y,vx,vy,ax,ay,speed,time,angle,y0,vx0,vy0,distance;
+    public MainAppMath(double y, double angle, double speed, double gravity) 
+    {
+        this.y0=y;
+        this.y = y;
+        this.ax = 0;
+        this.ay = gravity;
+        this.angle=angle;
+        this.speed = speed;
+        this.vx = this.speed*Math.cos(this.angle*(Math.PI/180.0));
+        this.vy = this.speed*Math.sin(this.angle*(Math.PI/180.0));
+        this.vx0 = this.speed*Math.cos(this.angle*(Math.PI/180.0));
+        this.vy0 = this.speed*Math.sin(this.angle*(Math.PI/180.0));
+        
+        this.time = (this.vy+Math.pow(Math.pow(this.vy, 2)+(4*0.5*this.ay*this.y), 0.5))/this.ay;
+        this.time=SF(this.time,3);
+        this.distance=this.vx0*this.time;
+        this.distance=SF(this.distance,3);
+    }
+    
+    public double SF(double value,int point)
+    {
+        value=value*Math.pow(10, point);
+        value=Math.floor(value);
+        value=value/Math.pow(10, point);
+        return value;
+    }
+    public double getCurrentV(double t)
+    {   
+        
+        return SF(Math.pow(Math.pow(this.vx0, 2)+Math.pow(this.vy0-this.ay*t, 2), 0.5),3);
+    }
+    public double getCurrentX(double t)
+    {   
+        return SF(this.vx0*t,3);
+    }
+    public double getCurrentY(double t)
+    {   
+        return SF(this.y0+(this.vy0*t-0.5*this.ay*t*t),3);
+    }
 }
