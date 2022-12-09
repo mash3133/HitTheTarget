659cd4aa2c99720789da8756d27ace0393d5b7f8
Maésha Mahmud <83089310+mash3133@users.noreply.github.com>
2022-12-09 09:24:58 -0500

    yggihu

--- HitTheTarget/app/src/main/java/edu/vanier/HitTheTarget/controllers/MainAppController.java
+++ HitTheTarget/app/src/main/java/edu/vanier/HitTheTarget/controllers/MainAppController.java
@@ -118,7 +118,6 @@ public class MainAppController {
         mousePtLabel.setText("X = " + (event.getX()) + "     Y = " + (event.getY()));
     }
     
-    
 
     //Change background depending on gravity chosen
     public void chosenGravity(ActionEvent event){
@@ -259,6 +258,28 @@ public class MainAppController {
             poly.getPoints().clear();
             btnStart.setDisable(false);
         }
+        
+        else{
+            Stage stage = new Stage();
+            this.pane = new GridPane();
+            Label lb = new Label();
+            lb.setText("please enter numbers");
+            lb.setAlignment(Pos.CENTER);
+            
+            this.pane.getChildren().add(lb);
+            
+            Scene scene = new Scene(pane, 300, 300);
+            stage.setScene(scene);
+            stage.setTitle("Error");
+            stage.show();
+            
+            pane.getChildren().clear();
+            currentTime = 0.0;
+            timeline.stop();
+            pt.stop();
+            poly.getPoints().clear();
+            btnStart.setDisable(false);
+        }
     }
     
     //Pauses animation
@@ -347,7 +368,7 @@ public class MainAppController {
                                                       
            
                             
-                                                                      
+                           
                             """);
         info.show();
         info.setResizable(true);
