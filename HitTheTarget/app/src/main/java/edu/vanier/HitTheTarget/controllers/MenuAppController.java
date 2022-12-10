df8c969e63193ffca1adac06036bb403c425d5b6
Maésha Mahmud <83089310+mash3133@users.noreply.github.com>
2022-12-09 19:21:55 -0500

    added a Driver class
    testing something, making it look more complicated

---
+++ HitTheTarget/app/src/main/java/edu/vanier/HitTheTarget/controllers/MenuAppController.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.vanier.HitTheTarget.controllers;
+
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author maesh
+ */
+public class MenuAppController {
+    Stage primaryStage;
+
+    public MenuAppController(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+
+    public MenuAppController() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+}
